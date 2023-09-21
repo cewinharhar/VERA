31a7544b72731bfa59a43c8b2620c49af1eace71
cewinharhar <kevin.yar@outlook.com>
2023-09-21 23:54:51 +0900

    remove max tokens

--- main/pages/api/getResponse.js
+++ main/pages/api/getResponse.js
@@ -15,8 +15,7 @@ const handler = async (req) => {
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
       messages,
-      temperature: 0.7,
-      max_tokens: 3072,
+      temperature: 0.7
     }),
   });
 };
