0bb9be0faf99680a06456a927f634fef838a40b2
cewinharhar <kevin.yar@outlook.com>
2023-09-28 14:01:47 +0900

    enginnering prompt

--- main/services/voiceBot.js
+++ main/services/voiceBot.js
@@ -78,9 +78,7 @@ export const languages = canUseWebSpeech
   A: General resistance to give consumers the driving seat in their health, especially in Europe. But regulators are relaxing as these technologies are proving beneficial for prevention.
   Q: If your team has knowledge and skill gaps, how will you fill them?
   A: We are currently searching for a third co-founder to fill our gap in business and marketing. 
-  Q: What is your hiring plan?
-  Q: Where do you see this business in five years? 
-  Q: What strategic partnerships are you considering to boost your growth and credibility? 
+  
   
   Science and Technology
   Q: What is the advantage of RNA and Protein in top of DNA?
@@ -92,13 +90,6 @@ export const languages = canUseWebSpeech
   A: There are many studies showing that multi-omics can predict cancer and even death up to 5 years before. PMID: 33004987 and PMID: 34145379.
   Mark Caulfield, chief scientist for Genomics England, says the project has multiple benefits, citing the example of a 10-year-old girl with severe recurrent chicken pox. “We found a change in her DNA which altered her immune system. This allowed us to select the bone marrow transplant which has cured her of her condition,” he says. “This is not only a transformation for the individual but it's also a huge saver of funds for the NHS, because she was recurrently being admitted and having intensive care.” https://www.bbc.com/worklife/article/20190301-how-screening-companies-are-monetising-your-dna
   
-  Q: How do you plan to stay ahead of technological advancements in the multi-omics field? 
-  Q: How does your startup adapt to the fast-changing landscape of bioinformatics?
-  Q: How are you ensuring the continuous improvement of your risk assessment models? 
-  Q: Are there any patents pending or in place to protect your technology and methodology?
-  Q: How do you ensure the intellectual property rights of the data providers (consumers) are not infringed upon? 
-  Q: How do you ensure the integrity and accuracy of the multi-omic data? 
-  Q: What's the backup strategy for the vast amount of data being generated and stored?
   Q: How frequently do you plan to update the risk assessment algorithms based on new research findings in the field? 
   Q: Are there any ongoing pilot projects or case studies showcasing the efficacy of your solution?
   Q: How do you intend to handle the surge in data storage requirements with increasing customers?
@@ -116,8 +107,6 @@ export const languages = canUseWebSpeech
   Q: What are the revenue and growth models?
   Q: How do you plan to use investor funds?
   Q: How you plan to use their money and how far it will take you? What milestones — particularly those related to growth and revenue — you will hit and when?
-  Q: How much money do you need for the 1st year? And first three years? If you get double that amount (or more), will you be faster?
-  Q: What's the payback period you anticipate for the investments? 
   Q: How do you price your product to both the consumer and the businesses? Are there tiered pricing models? 
   Q: Are there plans for diversifying revenue streams beyond the current model?
   
@@ -135,15 +124,11 @@ export const languages = canUseWebSpeech
   A: We will establish agreements with specialists in the sector (clinics and contractors).
   Q: Are there specific geographic markets you're targeting in the next phase? 
   A: In the next phase, we are focusing on high-density cities in Europe, specifically Paris, London, and Berlin. This approach will enhance our potential customer base by making the sample collection more accessible to them.
-  Q: How do you intend to handle localization challenges, both in terms of data regulation and market preferences?
-  Q: What channels have you set up to gather consumer feedback and concerns?
   Q: How do you plan to maintain consumer engagement and trust over the long term?
   A: By offering the annual re-analysis of the proteome we showcase improvements or setbacks which engages consumers to monitor their changes. Additionally, they will get a personalized E-mail report every 2-4 weeks containing information about new research concerning them as well as TrueYouOmics updates. 
   Q: Are there any feedback mechanisms in place for continuous product improvement based on consumer suggestions?
   A: Will be further evaluated during POC trial. 
   Q: What kind of post-purchase support do you offer to consumers who might have questions about their risk assessments?
-  Q: What has been the initial feedback from the beta testers or early adopters of your product?
-  Q: What's your onboarding process for new consumers? 
   
   Businesses
   Q: Have you discussed with businesses about their interest in this kind of data?
@@ -154,7 +139,8 @@ export const languages = canUseWebSpeech
   
   important: Keep responses short and in ${lang}.
   Instructions: 
-  Begin by asking the user for a seed integer to personalize the interaction. Once you have the seed integer, proceed with the questions. After each answer provided by the founder, give a short, professional feedback on the answer and provide guidance on areas of improvement`;
+  Begin by asking the user for a seed integer to personalize the interaction. Once you have the seed integer, proceed with the question, 1 by 1. After each answer provided by the founder, give a short, professional feedback on the answer and provide guidance on areas of improvement.
+  We start now:`;
 
 const voiceBot = ({
   messageOverride,
